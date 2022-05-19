ca20b8db476584d828b8df6eadbcd4f9a340da4a
Jung Tae Jun <ttajun@gmail.com>
2022-05-19 12:19:56 +0900

    add: README , common

--- app.py
+++ app.py
@@ -5,19 +5,25 @@ sys.path.append(os.path.dirname(__file__))
 # tensorflow warning 제거. tensorflow import 전에 실행해야 함 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
-from zUtil import registry, const, logger
+from common import const, logger
 log = logger.make_logger(__name__)
 
 
 def main():
     con = const.Const
-    reg = registry.registry
-    flow_name = con.FLOW_MLCC_TEXT
-    log.debug(f'flow_name: {flow_name}')
+    log.info(f'### codeML : version {con.VERSION}')
 
-    # flow class 생성
-    flo = reg[flow_name]()
-    flo.flow()
+    # Step 1. 수집 - Gather Data
+
+    # Step 2. 탐색 - Explore Data
+    
+    # Step 3. 준비 - Prepare Data (tokenize, vectorize)
+    
+    # Step 4. 모델 - Build, Train, and Evaluate Model
+    
+    # Step 5. 조정 - Tune Hyperparameters
+    
+    # Step 6. 배포 - Deploy Model
 
     return
 
---
+++ common/const.py
@@ -0,0 +1,7 @@
+
+class Const:
+
+    VERSION = '0.0.1'
+    DATA_DIR = 'data'
+    MODEL_DIR = 'model'
+
